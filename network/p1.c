532914d47153d72c93df63e3aa67ff3badc776ab
I0gan <l418894113@gmail.com>
2020-04-28 22:25:30 +0800

    net

--- network/p1.c
+++ network/p1.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+void byteorder() {
+	union {
+		short value;
+		char union_bytes[sizeof(short)];
+	} test;
+	test.value = 0x0102;
+	if((1 == test.union_bytes[0]) && (2 == test.union_bytes[1])) {
+		printf("big endian\n");
+	}else if((2 == test.union_bytes[0]) && (1 == test.union_bytes[1])) {
+		printf("little endian\n");
+	}else {
+		printf("unknown\n");
+	}
+}
+
 int main(void) {
-
-
+	byteorder();
 	return 0;
 }
